d50038bc5ef3eb1a171c672276b8fc09fe9d3576
Shinya Uryu <r.sh1nya.uryu@gmail.com>
2024-08-31 09:17:34 +0900

    Tweak

--- 04-renv.R
+++ 04-renv.R
@@ -3,25 +3,27 @@ library(renv)
 status()
 
 # コメントを外して renv::status()、renv::snapshot() を実行
+# (パッケージをインストールしないとコメントアウトされた箇所のコードは実行できない)
 
 status()
 
 snapshot()
-
+## 選択肢の入力を求められたら 2 を入力し実行する
+## renv.lockファイルの更新も Y で行う
+#
 # library(jmastats)
 # library(dplyr)
 # library(mapview)
 # library(sf)
-# stations |> 
+# stations |>
 #   mapview::mapview()
-# stations |> 
-#   filter(station_name == "徳島") |> 
-#   pull(block_no) |> 
+# stations |>
+#   filter(station_name == "徳島") |>
+#   pull(block_no) |>
 #   unique()
 # jma_collect(item = "hourly",
 #             block_no = "47895",
 #             year = 2024,
 #             month = 8,
-#             day = 29, 
+#             day = 29,
 #             cache = FALSE)
-# 
